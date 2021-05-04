12d059b52d5052e896eb948d8e8e409ed2cf1912
Brian <bweloba@gmail.com>
2021-05-04 21:58:31 +0300

    Add join table

--- src/main/resources/DB/create.sql
+++ src/main/resources/DB/create.sql
@@ -1,16 +1,25 @@
 SET MODE PostgreSQL;
 
-CREATE TABLE IF NOT EXISTS stores (
-    id int PRIMARY KEY auto_increment,
-    name VARCHAR,
+CREATE TABLE IF NOT EXISTS stores
+(
+    id      int PRIMARY KEY auto_increment,
+    name    VARCHAR,
     address VARCHAR,
     website VARCHAR
 );
 
-CREATE TABLE IF NOT EXISTS items (
+CREATE TABLE IF NOT EXISTS items
+(
+    id      int PRIMARY KEY auto_increment,
+    name    VARCHAR,
+    price   INTEGER,
+    storeid INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS stores_items
+(
     id int PRIMARY KEY auto_increment,
-    name VARCHAR,
-    price INTEGER,
+    itemid INTEGER,
     storeid INTEGER
 )
 
