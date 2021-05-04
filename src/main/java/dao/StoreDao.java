3a72e2f3b28cbd93b3b9b7dcd39a7563cd64a8d2
Brian <bweloba@gmail.com>
2021-05-04 22:00:58 +0300

    Add new interface methods

--- src/main/java/dao/StoreDao.java
+++ src/main/java/dao/StoreDao.java
@@ -1,5 +1,6 @@
 package dao;
 
+import models.Items;
 import models.Store;
 
 import java.util.List;
@@ -7,11 +8,12 @@ import java.util.List;
 public interface StoreDao {
     //create
     void add (Store store);
+    void addStoreToItem(Store store, Items item);
 
     //read
     List<Store> getAll();
     Store findById(int id);
-//    List<Items> getAllItemsInStore(int storeId);
+    List<Items> getAllItemsByStore(int storeId);
 
     //update
     void update(int id, String name, String address, String website);
