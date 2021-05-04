5fd74e7bbbc0d84640c03fbc6ee582c8db3128c4
Brian <bweloba@gmail.com>
2021-05-04 11:13:26 +0300

    Add tests for items and store classes

---
+++ src/test/java/models/ItemsTest.java
@@ -0,0 +1,68 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ItemsTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getName() {
+        Items testItems = setUpItems();
+        assertEquals("Bread", testItems.getName());
+    }
+
+    @Test
+    public void getPrice() {
+        Items testItems = setUpItems();
+        assertEquals(50, testItems.getPrice());
+    }
+
+    @Test
+    public void getStoreId() {
+        Items testItems = setUpItems();
+        assertEquals(1,testItems.getStoreId());
+    }
+
+    @Test
+    public void setName() {
+        Items testItems = setUpItems();
+        testItems.setName("Eggs");
+        assertNotEquals("Bread",testItems.getName());
+    }
+
+    @Test
+    public void setSoreId() {
+        Items testItems = setUpItems();
+        testItems.setStoreId(2);
+        assertNotEquals(1,testItems.getStoreId());
+    }
+
+    @Test
+    public void setPrice() {
+        Items testItems = setUpItems();
+        testItems.setPrice(20);
+        assertNotEquals(50,testItems.getPrice());
+    }
+
+    @Test
+    public void setAndGetId() {
+        Items testItems = setUpItems();
+        testItems.setId(1);
+        assertEquals(1,testItems.getId());
+    }
+
+    public Items setUpItems(){
+        return new Items("Bread",50,1);
+    }
+}
