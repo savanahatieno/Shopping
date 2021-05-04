43ba1de9196d75d9b861a5e700d51b2f2af1b259
Brian <bweloba@gmail.com>
2021-05-04 20:57:53 +0300

    fix bugs while creating database

--- src/main/resources/DB/create.sql
+++ src/main/resources/DB/create.sql
@@ -1,16 +1,17 @@
 SET MODE PostgreSQL;
 
 CREATE TABLE IF NOT EXISTS stores (
-    id int PRIMARY KEY auto_incriment,
+    id int PRIMARY KEY auto_increment,
     name VARCHAR,
     address VARCHAR,
     website VARCHAR
 );
 
 CREATE TABLE IF NOT EXISTS items (
-    id int PRIMARY KEY auto_incriment,
+    id int PRIMARY KEY auto_increment,
     name VARCHAR,
-    storeid NUMBER
+    price INTEGER,
+    storeid INTEGER
 )
 
 
